2d96d4168bcf3a0f1a158b35007cf7606e992f2f
Akee Marcus <Akeemarcus@gmail.com>
2025-08-27 15:40:28 -0400

    First push

---
+++ PasswordLibrary/PasswordLibrary.Test/PasswordCheckerTest.cs
@@ -0,0 +1,62 @@
+﻿using PasswordLibrary;
+using Xunit;
+
+namespace PasswordLibrary.Tests
+{
+    public class PasswordCheckerTester
+    {
+        [Fact]
+        public void EmptyPassword_ReturnsIneligible()
+        {
+            Assert.Equal("INELIGIBLE", PasswordChecker.CheckStrength(""));
+        }
+
+        [Fact]
+        public void OnlyUpperCase_ReturnsWeak()
+        {
+            Assert.Equal("WEAK", PasswordChecker.CheckStrength("BAHBAHBAH"));
+        }
+
+        [Fact]
+        public void OnlyLowerCase_ReturnsWeak()
+        {
+            Assert.Equal("WEAK", PasswordChecker.CheckStrength("password"));
+        }
+
+        [Fact]
+        public void OnlyDigits_ReturnsWeak()
+        {
+            Assert.Equal("WEAK", PasswordChecker.CheckStrength("12345"));
+        }
+
+        [Fact]
+        public void OnlySymbols_ReturnsWeak()
+        {
+            Assert.Equal("WEAK", PasswordChecker.CheckStrength("@#%$%$$"));
+        }
+
+        [Fact]
+        public void UpperAndLower_ReturnsMedium()
+        {
+            Assert.Equal("MEDIUM", PasswordChecker.CheckStrength("Password"));
+        }
+
+        [Fact]
+        public void LowerAndDigit_ReturnsMedium()
+        {
+            Assert.Equal("MEDIUM", PasswordChecker.CheckStrength("notpassword278"));
+        }
+
+        [Fact]
+        public void UpperLowerAndDigit_ReturnsMedium()
+        {
+            Assert.Equal("MEDIUM", PasswordChecker.CheckStrength("Maybepassword190"));
+        }
+
+        [Fact]
+        public void AllCriteria_ReturnsStrong()
+        {
+            Assert.Equal("STRONG", PasswordChecker.CheckStrength("GoodPassword35!"));
+        }
+    }
+}
---
+++ PasswordLibrary/PasswordLibrary/PasswordChecker.cs
@@ -0,0 +1,27 @@
+﻿namespace PasswordLibrary
+{
+    public static class PasswordChecker
+    {
+        public static string CheckStrength(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "INELIGIBLE";
+
+            bool hasUpper = password.Any(char.IsUpper);
+            bool hasLower = password.Any(char.IsLower);
+            bool hasDigit = password.Any(char.IsDigit);
+            bool hasSymbol = password.Any(ch => !char.IsLetterOrDigit(ch));
+
+            int criteriaMet = new[] { hasUpper, hasLower, hasDigit, hasSymbol }.Count(c => c);
+
+            return criteriaMet switch
+            {
+                0 => "INELIGIBLE",
+                1 => "WEAK",
+                2 or 3 => "MEDIUM",
+                4 => "STRONG",
+                _ => "INELIGIBLE"
+            };
+        }
+    }
+}
